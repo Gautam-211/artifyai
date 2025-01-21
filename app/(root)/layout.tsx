b3060faebe97880044c4211131bc344bd4bb5925
Gautam Sachdeva <gautamsachdeva20@gmail.com>
2025-01-22 00:52:25 +0530

    built sidebar and mobile navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
+import MobileNav from '@/components/shared/MobileNav'
+import SideBar from '@/components/shared/SideBar'
 import React from 'react'
 
 const Root = ({children} : {children : React.ReactNode}) => {
   return (
     <main className='root'>
+
+        <SideBar/>
+        <MobileNav/>
+
         <div className='root-container'>
             <div className='wrapper'>
                 {children}
